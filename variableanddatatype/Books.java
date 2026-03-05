16627c8d4f364a4bb8509085e154a99c74247e9e
Sandya26 <Sandhyasarode78@gmail.com>
2026-03-05 10:40:19 +0530

    variableanddatatype

---
+++ variableanddatatype/Books.java
@@ -0,0 +1,33 @@
+class Books
+{
+	static long prize=900L;
+	static	String publisher ="Pan Macmillan India";
+	static	String publicationDate=" 24 May 2019";
+	static	String language="English";
+	static	String printLength="384 pages";
+	static	String isbn="9386215969";
+	static	String itemWeight="0.34 g";
+	static	String dimensions ="2.2 x 12.9 x 19.81 cm";
+	static	String countryOfOrigin="India";
+	static	String netQuantity="1 Count";
+	static	String importer ="Pan Macmillan Publishing, 707, 7th Floor, Kailash Building 26, K.G. Marg, New Delhi, Delhi 110001";
+	static	String packer="Pan Macmillan Publishing, 707, 7th Floor, Kailash Building 26, K.G. Marg, New Delhi, Delhi 110001";
+	static	String genericName ="Book";
+	public static void main(String args[])
+	{
+
+		System.out.println("Book prize is: "+prize);
+		System.out.println("Publisher is: "+publisher);
+		System.out.println("Publication date is: "+publicationDate);
+		System.out.println("Language is: "+language);
+		System.out.println("Print length is: "+printLength);
+		System.out.println("ISBN-10 is: "+isbn);
+		System.out.println("Item Weight is: "+itemWeight);
+		System.out.println("Dimensions is: "+dimensions);
+		System.out.println("Country of Origin is: "+countryOfOrigin);
+		System.out.println("Net Quantity is: "+netQuantity);
+		System.out.println("Importer is: "+importer);
+		System.out.println("Packer is: "+packer);
+		System.out.println("Generic Name is: "+genericName);
+	}
+}
---
+++ variableanddatatype/TrolleyBag.java
@@ -0,0 +1,20 @@
+class TrolleyBag
+{
+	static long prize=14880L;
+	static	String department="womens";
+	static	String manufacturer="";
+	static	String countryOfOrigin="Bangladesh";
+	static	String itemModelNumber="B0G3X8C995";
+	static	String productDimensions="B0G3X8C995";
+	static	String asin="B0G3X8C995";
+	public static void main(String args[])
+	{
+		System.out.println("Prize of TrolleyBag is: "+prize);
+		System.out.println("Department is: "+department);
+		System.out.println("Manufacturer is: "+manufacturer);
+		System.out.println("Country of Origin is: "+countryOfOrigin);
+		System.out.println("Item model number is: "+itemModelNumber);
+		System.out.println("Product Dimensions is: "+productDimensions);
+		System.out.println("ASIN is: "+asin);
+	}
+}
