f48717ae7a9027256bde8f4b6f99473282e5f7b0
Sandya26 <Sandhyasarode78@gmail.com>
2026-03-05 11:44:11 +0530

    references example

---
+++ task3/Lays.java
@@ -0,0 +1,36 @@
+class Lays
+{
+	public static void main(String args[])
+	{
+		String laysIngredients[]={"Potato","Vegetable Oil","Salt","Onion powder","Garlic Powder","Maltodoxtrin","Citric acid"};
+		//System.out.println("Lays Ingredients are: " );
+		//System.out.println(laysIngredients[0]+"\n"+laysIngredients[1]+"\n"+laysIngredients[2]+"\n"+laysIngredients[3]+"\n"+
+		//laysIngredients[4]+"\n"+laysIngredients[5]+"\n"+laysIngredients[6]+"\n");
+		//for(String laysIngredient:laysIngredients){
+			/*System.out.println( laysIngredients[0]);
+			System.out.println( laysIngredients[1]);
+			System.out.println( laysIngredients[2]);
+			System.out.println( laysIngredients[3]);
+			System.out.println( laysIngredients[4]);
+			System.out.println( laysIngredients[5]);
+			System.out.println( laysIngredients[6]);*/
+			
+			
+			String  Potato =laysIngredients[0];
+			System.out.println(Potato);
+			String  VegetableOil =laysIngredients[1];
+			System.out.println(VegetableOil);
+			String  Salt  =laysIngredients[2];
+			System.out.println(Salt);
+			String  Onionpowder =laysIngredients[3];
+			System.out.println(Onionpowder);
+			String  GarlicPowder =laysIngredients[4];
+			System.out.println(GarlicPowder);
+			String  Maltodoxtrin =laysIngredients[5];
+			System.out.println(Maltodoxtrin);
+			String  Citricacid =laysIngredients[6];
+			System.out.println(Citricacid);
+			
+	}
+}
+
---
+++ task3/WorldCup.java
@@ -0,0 +1,72 @@
+class WorldCup
+{
+	public static void main(String args[])
+	{ 
+		String GroupA[]={"India","Australia","Canada","England"};
+		String GroupB[]={"Afganistan","Ireland","Italy","Namibia"};
+		String GroupC[]={"Nepal","Netherlands","New Zealand","Oman"};
+		String GroupD[]={"Pakistan","Scotland","South Africa","Sri Lanka"};
+		String GroupE[]={"USA","United Arab Emirates","West indies","Zimbabe"};
+		/*System.out.println("World Cup Group A members are: ");
+		System.out.println(GroupA[0] +"\n"+ 
+		GroupA[1] +"\n"+ 
+		GroupA[2] +"\n"+
+		GroupA[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group B members are: ");
+		System.out.println(GroupB[0] +"\n"+ GroupB[1] +"\n"+ GroupB[2] +"\n"+ GroupB[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group C members are: ");
+		System.out.println(GroupC[0] +"\n"+ GroupC[1] +"\n"+ GroupC[2] +"\n"+  GroupC[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group D members are: ");
+		System.out.println(GroupD[0] +"\n"+ GroupD[1] +"\n"+ GroupD[2] +"\n"+ GroupD[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group E members are: ");
+		System.out.println(GroupE[0] +"\n"+ GroupE[1] +"\n"+ GroupE[2] +"\n"+ GroupE[3]);
+					String  Potato =laysIngredients[0];
+			System.out.println(Potato);*/
+		String  India =GroupA[0];
+		System.out.println(India);
+		String  Australia =GroupA[1];
+		System.out.println(Australia);
+		String  Canada =GroupA[2];
+		System.out.println(Canada);
+		String  England =GroupA[3];
+		System.out.println(England);
+		String  Afganistan =GroupB[0];
+		System.out.println(Afganistan);
+		String  Ireland =GroupB[1];
+		System.out.println(Ireland);
+		String  Italy =GroupB[2];
+		System.out.println(Italy);
+		String  Namibia =GroupB[3];
+		System.out.println(Namibia);
+		String  Nepal =GroupC[0];
+		System.out.println(Nepal);
+		String  Netherlands =GroupC[1];
+		System.out.println(Netherlands);
+		String  NewZealand =GroupC[2];
+		System.out.println(NewZealand);
+		String  Oman =GroupC[3];
+		System.out.println(Oman);
+		String  Pakistan =GroupD[0];
+		System.out.println(Pakistan);
+		String  Scotland =GroupD[1];
+		System.out.println(Scotland);
+		String  SouthAfrica =GroupD[2];
+		System.out.println(SouthAfrica);
+		String  SriLanka =GroupD[3];
+		System.out.println(SriLanka);
+		String  USA =GroupE[0];
+		System.out.println(USA);
+		String  UnitedArabEmirates =GroupE[1];
+		System.out.println(UnitedArabEmirates);
+		String  Westindies =GroupE[2];
+		System.out.println(Westindies);
+		String  Zimbabe =GroupE[3];
+		System.out.println(Zimbabe);
+		
+		
+	}
+}
