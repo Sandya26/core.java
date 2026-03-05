0ed9c545bd9a7eaff1ef867f201b632ad58bc3a7
Sandya26 <Sandhyasarode78@gmail.com>
2026-03-05 16:55:06 +0530

    Looping examples

---
+++ loop/JioHotstar.java
@@ -0,0 +1,290 @@
+class JioHotstar
+{
+	public static void main(String args[])
+	{
+		String cnbcTv18 = "CNBC TV18";
+		String cnbcTv18Prime1 = "CNBC-TV18 Prime";
+		String cnbcAwaaz = "CNBC Awaaz";
+		String cnbcBajar1 = "CNBC Bajar";
+		String news18India = "News18 India";
+		String news18MpCg = "News18 MP Chattisgarh";
+		String news18Rajasthan = "News18 Rajasthan";
+		String news18TamilNadu = "News18 Tamil Nadu";
+		String news18Marathi = "News18 Marathi";
+		String news18UpUk = "News18 UP Uttarakhand";
+		String cnnNews18 = "CNN News18";
+		String cnbcTv18Prime2 = "CNBC-TV18 Prime";
+		String news18Kerala = "News18 Kerala";
+		String news18Bangla = "News18 Bangla";
+		String news18Gujarati = "News18 Gujarati";
+		String news18Odia = "News18 Odia";
+		String news18AssamNE1 = "News18 Assam NE";
+		String news18PunjabHaryana = "News18 Punjab Haryana";
+		String news18AssamNE2 = "News18 Assam NE";
+		String news18JKLH = "News18 JKLH";
+		String cnbcBajar2 = "CNBC Bajar";
+		String cnbc18 = "CNBC 18";
+		String publicTv = "Public TV";
+		String powerTv = "Power TV";
+		String tv9 = "TV9";
+		String suvarnaNews = "Suvarna News";
+		String aajTak = "Aaj Tak";
+		String abpNews = "ABP News";
+		String bharat24 = "Bharat 24";
+		String ddNews = "DD News";
+		String indiaNews = "India News";
+		String ndtvIndia = "NDTV India";
+
+		String liveBusinessNews[] = {
+			cnbcTv18, cnbcTv18Prime1, cnbcAwaaz, cnbcBajar1,
+			news18India, news18MpCg, news18Rajasthan,
+			news18TamilNadu, news18Marathi, news18UpUk,
+			cnnNews18, cnbcTv18Prime2, news18Kerala,
+			news18Bangla, news18Gujarati, news18Odia,
+			news18AssamNE1, news18PunjabHaryana,
+			news18AssamNE2, news18JKLH, cnbcBajar2,
+			cnbc18, publicTv, powerTv, tv9, suvarnaNews,
+			aajTak, abpNews, bharat24, ddNews,
+			indiaNews, ndtvIndia
+		};
+
+		System.out.println("Live Business & News Channels:");
+		for(String channel : liveBusinessNews)
+		{
+			System.out.println(channel);
+		}
+		System.out.println("------------------------------------");
+		String policePolice = "Police Police";
+		String robinhood = "Robinhood";
+		String mrsDeshpande = "Mrs.Deshpande";
+		String joePickett = "Joe Pickett";
+		String cricketJustice = "Cricket Justice";
+		String murderbaad = "Murderbaad";
+		String bloodyDaddy = "Bloody Daddy";
+		String search = "Search";
+		String siren = "Siren";
+		String ronth = "Ronth";
+		String thudarum = "Thudarum";
+		String kannurSquad = "Kannur Squad";
+		String sholay = "Sholay";
+		String aakhriSach = "Aakhri Sach";
+		String apurva = "Apurva";
+		String vikranthRona = "Vikranth Rona";
+		String heaven = "Heaven";
+		String quietPlachikkavu = "Quiet Plachikkavu";
+		String keralaCrimeFiles = "Kerala Crime Files";
+		String vikramVedha = "Vikram Vedha";
+		String monster = "Monster";
+		String khiladi = "Khiladi";
+		String commanderKaran = "Commander Karan Saxena";
+		String goodluckJerry = "Goodluck Jerry";
+		String underSaltMarsh = "Under Salt Marsh";
+		String sanak = "Sanak";
+		String sembi = "Sembi";
+		String fastAndFurious9 = "Fast & Furious9";
+		String aarya = "Aarya";
+
+		String crimeMovies[] = {
+			policePolice, robinhood, mrsDeshpande, joePickett, cricketJustice,
+			murderbaad, bloodyDaddy, search, siren, ronth,
+			thudarum, kannurSquad, sholay, aakhriSach, apurva,
+			vikranthRona, heaven, quietPlachikkavu, keralaCrimeFiles, vikramVedha,
+			monster, khiladi, commanderKaran, goodluckJerry, underSaltMarsh,
+			sanak, sembi, fastAndFurious9, aarya
+		};
+
+		System.out.println("Crime Movies & Series List:");
+		for(String movie : crimeMovies)
+		{
+			System.out.println(movie);
+		}
+		System.out.println("------------------------------------");
+		String mermaidDown = "Mermaid Down";
+		String raid = "Raid";
+		String kaabil = "Kaabil";
+		String theGrey = "The Grey";
+		String airlift = "Airlift";
+		String villain = "Villain";
+		String kee = "KEE";
+		String maatr = "Maatr";
+		String maayavan = "Maayavan";
+		String laalRang = "Laal Rang";
+		String aakhiriChaal = "Aakhiri Chaal";
+		String iSpitOnYourGrave = "I Spit on Your Grave";
+		String welcomeHome = "Welcome Home";
+		String ugly = "Ugly";
+		String theThieves = "The Thieves";
+		String iSeeYou = "I See You";
+		String d16 = "D-16";
+		String eightThottakkal = "8 Thottakkal";
+		String monsoonShootout = "Monsoon Shootout";
+		String security = "Security";
+		String dongriKaRaja = "Dongri Ka Raja";
+		String omerta = "Omerta";
+		String copy = "COPY";
+		String jaalEkTantra = "Jaal Ek Tantra";
+		String khatronSeKhelenge = "Khatron Se Khelenge";
+		String badSamaritan = "Bad Samaritan";
+		String mirchiMalin = "Mirchi Malin";
+		String betweenWords = "Between Words";
+		String hateStory2 = "Hate Story 2";
+		String belzebuth = "Belzebuth";
+
+		String thrillMovies[] = {
+			mermaidDown, raid, kaabil, theGrey, airlift,
+			villain, kee, maatr, maayavan, laalRang,
+			aakhiriChaal, iSpitOnYourGrave, welcomeHome,
+			ugly, theThieves, iSeeYou, d16, eightThottakkal,
+			monsoonShootout, security, dongriKaRaja, omerta,
+			copy, jaalEkTantra, khatronSeKhelenge, badSamaritan,
+			mirchiMalin, betweenWords, hateStory2, belzebuth
+		};
+
+		System.out.println("Thriller Movies on Amazon Prime:");
+		for(String movie : thrillMovies)
+		{
+			System.out.println(movie);
+		}
+		System.out.println("------------------------------------");
+		String shootoutLokhandwala = "Shootout At Lokhandwala";
+		String theDescent1 = "The Descent";
+		String karz = "Karz";
+		String jeepers2 = "Jeepers 2";
+		String garv = "Garv";
+		String krishnaCottage = "Krishna Cottage";
+		String lajja = "lajja";
+		String theDescent2 = "The Descent part2";
+		String aan = "Aan";
+		String bhoot = "Bhoot";
+		String deewangee = "Deewangee";
+		String stoneman = "The Stoneman";
+		String shutter = "Shutter";
+		String dawnOfDead = "Dawn of the Dead";
+		String thirteenB = "13 B";
+		String insideMan = "Inside Man";
+		String ring = "Ring";
+		String bourneIdentity = "Bourne Identity";
+		String chandramukhi = "Chandramukhi";
+		String liveFreeDieHard = "Live Free or Die hard";
+		String gayatri1920 = "1920 Gayatri";
+		String changeling = "Changeling";
+		String stateOfPlay = "State of Play";
+		String hannibal = "Hannibai";
+		String redDragon = "Red Dragon";
+		String untraceable = "Untraceble";
+		String darnaMana = "darna mana";
+		String shikari = "Shikari";
+		String sikandar = "Sikandar";
+		String kuchToHai = "Kucch To Hai";
+		String eightyEightMinutes = "88 Minutes";
+
+		String devilsMovie[] = {
+			shootoutLokhandwala, theDescent1, karz, jeepers2, garv,
+			krishnaCottage, lajja, theDescent2, aan, bhoot,
+			deewangee, stoneman, shutter, dawnOfDead, thirteenB,
+			insideMan, ring, bourneIdentity, chandramukhi, liveFreeDieHard,
+			gayatri1920, changeling, stateOfPlay, hannibal, redDragon,
+			untraceable, darnaMana, shikari, sikandar, kuchToHai,
+			eightyEightMinutes
+		};
+
+		System.out.println("Devils / Horror / Thriller Movies List:");
+		for(String movie : devilsMovie)
+		{
+			System.out.println(movie);
+		}
+		System.out.println("------------------------------------");
+		String eyesOfWakanda = "Eyes of Wakanda";
+		String captainAmerica1 = "Captain America";
+		String captainMarvel = "Captain Marvel";
+		String ironMan1 = "Iron Man";
+		String ironMan2 = "Iron Man 2";
+		String thor1 = "Thor";
+		String avengers = "Avengers";
+		String thor2 = "Thor";
+		String ironMan3 = "Iron Man 3";
+		String captainAmerica2 = "Captain America";
+		String gotg1 = "Guardians of the Galaxy";
+		String gotg2 = "Guardians of the Galaxy 2";
+		String iAmGroot = "I Am Groot";
+		String daredevil = "Daredevil";
+		String jessicaJones = "Jessica Jones";
+		String antMan = "Ant-Man";
+		String ironFist = "Iron Fist";
+		String defenders = "Defenders";
+		String civilWar = "Civil War";
+		String blackWidow = "Black Widow";
+		String blackPanther = "Black Panther";
+		String punisher = "Punisher";
+		String doctorStrange = "Doctor Strange";
+		String thorRagnarok = "Thor Ragnarok";
+		String echo = "Echo";
+		String thorLoveThunder = "Thor Love and Thunder";
+		String agathaAllAlong = "Agatha All Along";
+		String gotgVol2 = "The Guardians of the Galaxy 2";
+		String deadpoolWolverine = "Deadpool & Wolverine";
+		String thunderBolts = "Thunder Bolts";
+
+		String mcuTimeline[] = {
+			eyesOfWakanda, captainAmerica1, captainMarvel, ironMan1, ironMan2,
+			thor1, avengers, thor2, ironMan3, captainAmerica2,
+			gotg1, gotg2, iAmGroot, daredevil, jessicaJones,
+			antMan, ironFist, defenders, civilWar, blackWidow,
+			blackPanther, punisher, doctorStrange, thorRagnarok,
+			echo, thorLoveThunder, agathaAllAlong, gotgVol2,
+			deadpoolWolverine, thunderBolts
+		};
+
+		System.out.println("MCU Timeline:");
+		for(String movie : mcuTimeline)
+		{
+			System.out.println(movie);
+		}
+		System.out.println("------------------------------------");
+		String tangled = "Tangled";
+		String maleficient = "Maleficient";
+		String frozen2 = "Frozen2";
+		String narnia = "Narnia";
+		String princessDiaries = "The Princess Diaries";
+		String moana = "Moana";
+		String frozen = "Frozen";
+		String mulan = "Mulan";
+		String aliceWonderland = "Alice in Wonderland";
+		String enchanted = "Enchanted";
+		String cinderella = "Cinderella";
+		String tinkerBellNeverbeast = "Tinker Bell Neverbeast";
+		String littleMermaid = "The Little Mermaid";
+		String alice = "Alice";
+		String aladdin = "Alladdin";
+		String tinkerBell = "Tinker Bell";
+		String beautyBeast = "Beauty and the Beast";
+		String princessFrog = "Princess Rrog";
+		String tinkerBellLostTreasure = "Tinker Bell Lost Treasure";
+		String sleepingBeauty = "Sleeping Beauty";
+		String elenaAvalor = "Elena of Avalor";
+		String tangledSeries = "Tangled the Series";
+		String ariel = "Ariel";
+		String hocusPocus2 = "Hocus Pocus 2";
+		String snowWhite = "Snow White";
+		String castleQuest = "The Castle Quest";
+		String pocahontas = "Pocahontas";
+		String tinkerBellGreatRescue = "Tinker Bell Great fairy Resue";
+		String maleficient2 = "Maleficient Mistress of Evil";
+
+		String fairyTales[] = {
+			tangled, maleficient, frozen2, narnia, princessDiaries,
+			moana, frozen, mulan, aliceWonderland, enchanted,
+			cinderella, tinkerBellNeverbeast, littleMermaid, alice, aladdin,
+			tinkerBell, beautyBeast, princessFrog, tinkerBellLostTreasure, sleepingBeauty,
+			elenaAvalor, tangledSeries, ariel, hocusPocus2, snowWhite,
+			castleQuest, pocahontas, tinkerBellGreatRescue, maleficient2
+		};
+
+		System.out.println("Fairy Tales Movies & Series List:");
+		for(String story : fairyTales)
+		{
+			System.out.println(story);
+		}
+		System.out.println("------------------------------------");
+	}
+}
