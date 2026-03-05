9a45ebcefe97ba895c192304421f093292c0aac4
Sandya26 <Sandhyasarode78@gmail.com>
2026-03-05 10:14:49 +0530

    task2

---
+++ task2/Cafe.java
@@ -0,0 +1,39 @@
+class Cafe
+{
+	public static void main(String args[])
+	{
+		String cafeName="Cringe Cafe";
+		String teaNames[]={"Masala Chaha","Normal Chaha","Lemon Chaha","Elachi Chaha","Ginger Chaha","Green Chaha",""};
+		String coffeeNames[]={"Lattes","Cappuccino","Americanos","Mocha","Long Black",
+		"Black Coffee","Cold Brew","Red Eye","Irish Coffee","Turkish Coffee",
+		"Arabica","Robusta","Liberica","Excelsa"};
+		String snacksNames[]={"Nuts","Greek yogurt","Apple slices with peanut butter",
+		"Veggie stick with hummus","Protein-Packed","Chips","Indian Snack"};
+		System.out.println("Tea Names are Listed: ");
+		System.out.println(teaNames[0]+ "\n" +
+		teaNames[1]+ "\n" +
+		teaNames[2]+ "\n" +
+		teaNames[3]+ "\n" +
+		teaNames[4]);
+		System.out.println("---------------------");
+		System.out.println("Coffee Names are Listed: ");
+		System.out.println(coffeeNames[0]+ "\n" +
+		coffeeNames[1]+ "\n" +
+		coffeeNames[2]+ "\n" +
+		coffeeNames[4]+ "\n" +
+		coffeeNames[5]+ "\n"+
+		coffeeNames[6]+ "\n"+
+		coffeeNames[7]);
+		System.out.println("---------------------");
+		System.out.println("Snacks Names are Listed: ");
+		System.out.println(snacksNames[0]+"\n"+
+		snacksNames[1]+"\n"+
+		snacksNames[2]+"\n"+
+		snacksNames[3]+"\n"+
+		snacksNames[4]+"\n"+
+		snacksNames[5]+"\n"+
+		snacksNames[6]+"\n");
+		
+	}
+	
+}
---
+++ task2/TTwentyMatch.java
@@ -0,0 +1,25 @@
+class TTwentyMatch
+{
+	public static void main(String args[])
+	{
+		String GroupA[]={"IND","USA","PAK","NED","NAM"};
+		String GroupB[]={"SL","ZIM","AUS","IRE","OMA"};
+		String GroupC[]={"WI","ENG","SCO","ITA","NEP"};
+		String GroupD[]={"SA","NZ","AFG","UAE","CAN"};
+		System.out.println("T Twenty Match Group A members are: ");
+		System.out.println(GroupA[0] +"\n"+ 
+		GroupA[1] +"\n"+ 
+		GroupA[2] +"\n"+
+		GroupA[3]);
+		System.out.println("------------------");
+		System.out.println("T Twenty Match Group B members are: ");
+		System.out.println(GroupB[0] +"\n"+ GroupB[1] +"\n"+ GroupB[2] +"\n"+ GroupB[3]);
+		System.out.println("------------------");
+		System.out.println("T Twenty Match Group C members are: ");
+		System.out.println(GroupC[0] +"\n"+ GroupC[1] +"\n"+ GroupC[2] +"\n"+  GroupC[3]);
+		System.out.println("------------------");
+		System.out.println("T Twenty Match Group D members are: ");
+		System.out.println(GroupD[0] +"\n"+ GroupD[1] +"\n"+ GroupD[2] +"\n"+ GroupD[3]);
+		System.out.println("------------------");
+	}
+}
