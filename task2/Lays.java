4487860e4fff0f36a5c47114da04a7caa30d56c3
Sandya26 <Sandhyasarode78@gmail.com>
2026-03-05 11:22:42 +0530

    array reference examples

---
+++ task2/Lays.java
@@ -0,0 +1,11 @@
+class Lays
+{
+	public static void main(String args[])
+	{
+		String laysIngredients[]={"Potato","Vegetable Oil","Salt","Onion powder","Garlic Powder","Maltodoxtrin","Citric acid"};
+		System.out.println("Lays Ingredients are: " );
+		System.out.println(laysIngredients[0]+"\n"+laysIngredients[1]+"\n"+laysIngredients[2]+"\n"+laysIngredients[3]+"\n"+
+		laysIngredients[4]+"\n"+laysIngredients[5]+"\n"+laysIngredients[6]+"\n");
+	}
+
+}
---
+++ task2/WorldCup.java
@@ -0,0 +1,28 @@
+class WorldCup
+{
+	public static void main(String args[])
+	{ 
+		String GroupA[]={"India","Australia","Canada","England"};
+		String GroupB[]={"Afganistan","Ireland","Italy","Namibia"};
+		String GroupC[]={"Nepal","Netherlands","New Zealand","Oman"};
+		String GroupD[]={"Pakistan","Scotland","South Africa","Sri Lanka"};
+		String GroupE[]={"USA","United Arab Emirates","Weat indies","Zimbabwe"};
+		System.out.println("World Cup Group A members are: ");
+		System.out.println(GroupA[0] +"\n"+ 
+		GroupA[1] +"\n"+ 
+		GroupA[2] +"\n"+
+		GroupA[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group B members are: ");
+		System.out.println(GroupB[0] +"\n"+ GroupB[1] +"\n"+ GroupB[2] +"\n"+ GroupB[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group C members are: ");
+		System.out.println(GroupC[0] +"\n"+ GroupC[1] +"\n"+ GroupC[2] +"\n"+  GroupC[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group D members are: ");
+		System.out.println(GroupD[0] +"\n"+ GroupD[1] +"\n"+ GroupD[2] +"\n"+ GroupD[3]);
+		System.out.println("------------------");
+		System.out.println("World Cup Group E members are: ");
+		System.out.println(GroupE[0] +"\n"+ GroupE[1] +"\n"+ GroupE[2] +"\n"+ GroupE[3]);
+	}
+}
